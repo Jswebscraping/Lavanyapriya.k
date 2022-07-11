b68784029def8c16ad7c2ceca70d6adcea2661ca
Jswebscraping <97273713+Jswebscraping@users.noreply.github.com>
2022-07-11 11:23:26 +0530

    Delete regex1st.js

--- regex1st.js
+++
@@ -1,4 +0,0 @@
-const regex1 = RegExp('1159*');
-var url = 'https://www.chemistwarehouse.co.nz/buy/1159/betadine-sore-throat-ready-to-use-120ml';
-const result=regex1.exec(url);
-console.log(result[0]); 
